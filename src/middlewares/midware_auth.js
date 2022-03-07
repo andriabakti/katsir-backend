87f16af6856b85b20f47d26845b11262fa5b2ee4
Andria Bakti Mahendra <andr.bkti@gmail.com>
2022-03-07 23:02:28 +0700

    update middleware: auth

--- src/middlewares/midware_auth.js
+++ src/middlewares/midware_auth.js
@@ -4,7 +4,7 @@ const { response } = require('../helpers/helper_resp')
 module.exports = {
   verifyAccess: (req, res, next) => {
     let token = req.headers.authorization.split(' ')[1]
-    verify(token, process.env.SECRET_KEY, (err, _decoded) => {
+    verify(token, process.env.JWT_KEY, (err, _decoded) => {
       const message = 'Token is invalid'
       if (err) {
         return response(res, [], 403, message, null, err)
