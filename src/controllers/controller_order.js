8797cd7201eaf0ba580e04006c36c4169ed58009
Andria Bakti Mahendra <andr.bkti@gmail.com>
2022-03-15 02:17:04 +0700

    add uploaded photos and update git: ignore, middleware: auth & controllers

--- src/controllers/controller_order.js
+++ src/controllers/controller_order.js
@@ -1,53 +1,42 @@
 // model: order
-const {
-  insertOrderDetail,
-  insertOrderItem,
-  getAllOrder
-} = require("../models/model_order")
+const { insertOrderDetail, insertOrderItem, getAllOrder } = require("../models/model_order")
 // helper: response
-const {
-  response
-} = require("../helpers/helper_resp")
+const { response } = require("../helpers/helper_resp")
 
 module.exports = {
-  createOrder: async (req, res, next) => {
-    const {
-      id,
-      total,
-      tax,
-      items
-    } = req.body
-    const payload = {
-      user_id: id,
-      total,
-      tax,
-      created_at: new Date()
-    }
-    try {
-      const detail = await insertOrderDetail(payload)
-      console.log(detail)
-      await insertOrderItem(detail.insertId, id, items)
-      response(res, [], res.statusCode, "Order(s) created successfully", null, null)
-    } catch (error) {
-      console.log(error)
-      next({
-        status: error.statusCode,
-        message: "Failed to create the order(s)"
-      })
-    }
-  },
-  readAllOrder: (_req, res, next) => {
-    getAllOrder()
-      .then((result) => {
-        console.log(result)
-        response(res, result, res.statusCode, "Orders found", null, null)
-      })
-      .catch((error) => {
-        console.log(error)
-        next({
-          status: error.statusCode,
-          message: "Orders not found"
-        })
-      })
-  }
+	createOrder: async (req, res, next) => {
+		const { id, total, tax, items } = req.body
+		const payload = {
+			user_id: id,
+			total,
+			tax,
+			created_at: new Date()
+		}
+		try {
+			const detail = await insertOrderDetail(payload)
+			console.log(detail)
+			await insertOrderItem(detail.insertId, id, items)
+			response(res, [], res.statusCode, "Order(s) created successfully", null, null)
+		} catch (error) {
+			console.log(error)
+			next({
+				status: error.statusCode,
+				message: "Failed to create the order(s)"
+			})
+		}
+	},
+	readAllOrder: (_req, res, next) => {
+		getAllOrder()
+			.then((result) => {
+				console.log(result)
+				response(res, result, res.statusCode, "Orders found", null, null)
+			})
+			.catch((error) => {
+				console.log(error)
+				next({
+					status: error.statusCode,
+					message: "Orders not found"
+				})
+			})
+	}
 }
--- src/controllers/controller_user.js
+++ src/controllers/controller_user.js
@@ -8,59 +8,59 @@ const { createUser, getUserByEmail } = require("../models/model_user")
 const { response } = require("../helpers/helper_resp")
 
 module.exports = {
-  signUp: (req, res, next) => {
-    const { email, password, username, role } = req.body
-    const salt = bcrypt.genSaltSync(10)
-    const hash = bcrypt.hashSync(password, salt)
-    const data = {
-      email,
-      password: hash,
-      username,
-      role,
-      created_at: new Date()
-    }
-    createUser(data)
-      .then((result) => {
-        response(res, result, 201, "Register success", null, null)
-      })
-      .catch((error) => {
-        console.log(error)
-        next({
-          status: error.statusCode,
-          message: "Failed to register new user"
-        })
-      })
-  },
-  signIn: (req, res, next) => {
-    const { email, password } = req.body
-    getUserByEmail(email)
-      .then((result) => {
-        if (result.length < 1) {
-          return next({ status: 404, message: "User not found" })
-        }
-        const user = result[0]
-        bcrypt.compare(password, user.password).then((resCompare) => {
-          !resCompare && next({ status: 404, message: "Password is wrong" })
-          const payload = {
-            id: user.id,
-            email: user.email,
-            role: user.role
-          }
-          jwt.sign(payload, process.env.JWT_KEY, { expiresIn: "12h" }, (_err, token) => {
-            user.token = token
-            delete user.password
-            delete user.created_at
-            delete user.updated_at
-            response(res, result[0], res.statusCode, "Login success", null, null)
-          })
-        })
-      })
-      .catch((error) => {
-        console.log(error)
-        next({
-          status: error.statusCode,
-          message: "Failed to login"
-        })
-      })
-  }
+	signUp: (req, res, next) => {
+		const { email, password, username, role } = req.body
+		const salt = bcrypt.genSaltSync(10)
+		const hash = bcrypt.hashSync(password, salt)
+		const data = {
+			email,
+			password: hash,
+			username,
+			role,
+			created_at: new Date()
+		}
+		createUser(data)
+			.then((result) => {
+				response(res, result, 201, "Register success", null, null)
+			})
+			.catch((error) => {
+				console.log(error)
+				next({
+					status: error.statusCode,
+					message: "Failed to register new user"
+				})
+			})
+	},
+	signIn: (req, res, next) => {
+		const { email, password } = req.body
+		getUserByEmail(email)
+			.then((result) => {
+				if (result.length < 1) {
+					return response(res, [], 404, "User not found", null, null)
+				}
+				const user = result[0]
+				bcrypt.compare(password, user.password).then((resCompare) => {
+					!resCompare && response(res, [], 404, "Password is wrong", null, null)
+					const payload = {
+						id: user.id,
+						email: user.email,
+						role: user.role
+					}
+					jwt.sign(payload, process.env.JWT_KEY, { expiresIn: "12h" }, (_err, token) => {
+						user.token = token
+						delete user.password
+						delete user.created_at
+						delete user.updated_at
+						response(res, result[0], res.statusCode, "Login success", null, null)
+					})
+				})
+			})
+			.catch((error) => {
+				console.log(error)
+				next({
+					status: error.statusCode,
+					message: "Failed to login"
+				})
+			})
+	}
 }
