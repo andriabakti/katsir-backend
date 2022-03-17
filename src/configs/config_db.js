06fe31490b2e8d638bddfdf8f335a9373240d9a1
Andria Bakti Mahendra <andr.bkti@gmail.com>
2022-03-17 08:06:43 +0700

    edit config: db

--- src/configs/config_db.js
+++ src/configs/config_db.js
@@ -2,12 +2,12 @@
 const { Pool } = require("pg")
 
 const pool = new Pool({
-  host: process.env.PG_HOST,
-  user: process.env.PG_USER,
-  password: process.env.PG_PASSWORD,
-  database: process.env.PG_DATABASE,
-  port: process.env.PG_PORT,
-  // connectionString: `${process.env.DATABASE_URL}?sslmode=no-verify`,
-  // ssl: { rejectUnauthorized: false }
+  // host: process.env.PG_HOST,
+  // user: process.env.PG_USER,
+  // password: process.env.PG_PASSWORD,
+  // database: process.env.PG_DATABASE,
+  // port: process.env.PG_PORT,
+  connectionString: `${process.env.DATABASE_URL}?sslmode=no-verify`,
+  ssl: { rejectUnauthorized: false }
 })
 module.exports = pool
